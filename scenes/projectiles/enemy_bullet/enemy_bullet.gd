c510d352ed923f3c92f086f11ccbc73bcb8d7234
Artem Topolskyi <artem.t@mate.academy>
2025-11-30 00:09:24 +0200

    make enemy shoot bullets with some interval

---
+++ scenes/projectiles/enemy_bullet/enemy_bullet.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+@export var speed: float = 1000.0;
+
+func _ready() -> void:
+  $VisibleOnScreenNotifier2D.screen_exited.connect(_on_screen_exited);
+  $Hitbox.hit.connect(_on_hit);
+
+
+func _physics_process(delta: float) -> void:
+  position.y += speed * delta;
+
+
+func _on_screen_exited() -> void:
+  queue_free();
+
+
+func _on_hit() -> void:
+  queue_free();
